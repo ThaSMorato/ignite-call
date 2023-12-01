6b8872f6e90ad3ed60ea7698c47c56b1e4251e35
ThaSMorato <tha.s.morato@gmail.com>
2023-12-01 19:43:47 -0300

    feat: claim username component and styles

---
+++ src/pages/home/components/ClaimUsernameForm.tsx
@@ -0,0 +1,54 @@
+import { Button, Text, TextInput } from '@thasmorato-ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Username com 3 letras ou mais' })
+    .regex(/^[a-z\\-]+$/i, { message: 'Username apenas letras e hifens' })
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
+export const ClaimUsernameForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  const handleClaimUsername = async (data: ClaimUsernameFormData) => {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="username"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o username desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
